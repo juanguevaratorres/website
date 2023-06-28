8ae9212b426beefd25ac59fcd97ffc9574550347
juanguevaratorres <138071457+juanguevaratorres@users.noreply.github.com>
2023-06-28 17:05:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var firstname =  "juan"
+ var lastname =  "jr"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+ var numberOfSiblings= 2;
+ var hasPets = false;
+
+
+  console.log(firstname)
+  console.log(lastname) 
+  console.log(numberOfSiblings)
+  console.log(hasPets)
+
+ 
+
+  var button = document.getElementById("hi");
+button.addEventListener("click", function(){
+  console.log("click!");
+
+});
+
+ 
+
+
+
+
+
+
+
+
+
+
